8f58f0e0ff6e989c02ed300b6cb6aa8bb0a738e1
murgupluoglu <masadov@gmail.com>
2020-06-26 10:27:36 +0300

    DragShadowBuilder bug fixed

--- app/src/main/java/com/murgupluoglu/timetablelib/EmptyDragShadowBuilder.kt
+++ app/src/main/java/com/murgupluoglu/timetablelib/EmptyDragShadowBuilder.kt
@@ -8,7 +8,8 @@ class EmptyDragShadowBuilder(arg0: View) : View.DragShadowBuilder(arg0) {
 
     override fun onProvideShadowMetrics(size: Point, touch: Point) {
         super.onProvideShadowMetrics(size, touch)
-
+        size.set(1,1)
+        touch.set(0,0)
     }
 
     override fun onDrawShadow(canvas: Canvas) {
